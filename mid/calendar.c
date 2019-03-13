b47756a2a8f8b0a1a6ed6a6159ef035be0737cbf
ryosuke4138 <ryoryo4138@gmail.com>
2019-03-13 20:12:23 +0900

    calendar

---
+++ mid/calendar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int mday[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int dayofweek(int year, int month, int day)
+{
+	if (month== 1 || month == 2) {
+		year--;
+		month += 12;
+	}
+	return ((year + year/4 - year/100 + year/400 + (13*month+8)/5 + day) % 7);
+}
+
+int main(void)
+{
+	int y, m;
+
+	return 0;
+}
+
